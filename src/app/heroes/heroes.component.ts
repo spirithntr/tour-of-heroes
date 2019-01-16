a81c232f59bcb76e5b79c713ad0c0a69150fdcc5
Ivan <spirithntr@gmail.com>
2019-01-16 17:30:03 +0400

    finish 'displaying a list' lesson

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,11 +8,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  
   heroes = HEROES;
-
+  selectedHero: Hero;
+  
+  
   constructor() { }
   
   ngOnInit() {
   }
-
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
